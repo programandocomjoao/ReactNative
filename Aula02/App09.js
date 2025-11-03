0ff222ed46b7c3d7045f1e17015f79e493907175
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-11-03 19:04:55 -0300

    Add files via upload

--- Aula02/App09.js
+++ Aula02/App09.js
@@ -3,11 +3,11 @@ import { View } from 'react-native'
 
 const App = () => {
   return(
-    <>
+    <View style={{ flex: 1 }}>
       <View style={{ backgroundColor: 'silver', flex: 3 }}></View>
       <View style={{ backgroundColor: 'gray', flex: 5 }}></View>
       <View style={{ backgroundColor: 'black', flex: 2 }}></View>
-    </>
+    </View>
   )
 }
 
