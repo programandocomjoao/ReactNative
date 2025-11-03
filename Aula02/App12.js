b5de72cf7436a1d98eebfc6b3d5834b1e0010259
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-11-03 19:19:01 -0300

    Add files via upload

---
+++ Aula02/App12.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, StyleSheet } from 'react-native'
+
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  }
+})
+
+const App = () => {
+  return(
+    <View style={ estilos.container }>
+      <View style={{ backgroundColor: 'red', width: 75, height: 75 }}></View>
+      <View style={{ backgroundColor: 'yellow', width: 75, height: 75 }}></View>
+      <View style={{ backgroundColor: 'orange', width: 75, height: 75 }}></View>
+    </View>
+  )
+}
+
+export default App
