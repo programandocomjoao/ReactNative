cc2010f0c25ab02c829bff1b0fd6d71d1069c8b6
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-19 20:57:48 -0300

    Add files via upload

--- Aulas20261/Aula03/App.js
+++ Aulas20261/Aula03/App.js
@@ -1,1 +1,20 @@
+import React from 'react'
+import Home from './components/Home'
+import Midias from './components/Midias'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
+const Pilha = createStackNavigator()
+
+const App = () => {
+  return(
+    <NavigationContainer>
+      <Pilha.Navigator initialRouteName="Home">
+        <Pilha.Screen name="Home" component={ Home } />
+        <Pilha.Screen name="Mídias Sociais" component={ Midias } />
+      </Pilha.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
