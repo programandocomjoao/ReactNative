cd10deaef5732128d1877e8d2bf59c120ef34a81
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-19 20:58:36 -0300

    Add files via upload

--- Aulas20261/Aula03/components/Estilos.js
+++ Aulas20261/Aula03/components/Estilos.js
@@ -1,1 +1,16 @@
+import { StyleSheet } from 'react-native'
 
+const Estilos = StyleSheet.create({
+  principal: { flex: 1, justifyContent: 'flex-start', alignItems: 'center', padding: 20 },
+  logo: { width: 150, height: 47, marginTop: 30, marginBottom: 30 },
+  titulo: { fontWeight: 'bold', fontSize: 24 },
+  paragrafo: { paddingTop: 20, paddingBottom: 20 },
+  container: { flex: 1 },
+  cabecalho: { flex: 3, justifyContent: 'center', alignItems: 'center' },
+  subcontainer: { flex: 3, flexDirection: 'row', alignItems: 'stretch' },
+  rodape: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  coluna: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  icone: { width: 75, height: 75 }
+})
+
+export default Estilos
