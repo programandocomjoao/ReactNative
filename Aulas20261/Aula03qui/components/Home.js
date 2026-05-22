6ff81711e35d477999d61896655bd36560fbfc3e
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-21 21:19:33 -0300

    Add files via upload

--- Aulas20261/Aula03qui/components/Home.js
+++ Aulas20261/Aula03qui/components/Home.js
@@ -1,1 +1,25 @@
+import React from 'react'
+import { View, Image, Text, Button } from 'react-native'
+import Estilos from './Estilos'
 
+const Home = ({ navigation }) => {
+  return(
+    <View style={ Estilos.principal }>
+      <Image
+        source={ require('../assets/logoUnilasalle.png') }
+        style={ Estilos.logo }  
+      />
+
+      <Text style={ Estilos.titulo }>Nossa Missão e Valores</Text>
+
+      <Text style={ Estilos.paragrafo }>A missão do La Salle é "transformar vidas com excelência educativa". A universidade busca formar profissionais integralmente, com ética e senso crítico, preparando-os para atuar na sociedade de forma inovadora e sustentável. Isso é realizado através de uma pedagogia que promove a produção, apropriação e socialização do conhecimento.</Text>
+
+      <Button
+        title="Mídias Sociais"
+        onPress={ () => navigation.navigate('Mídias Sociais') }
+      />
+    </View>
+  )
+}
+
+export default Home
---
+++ Aulas20261/Aula03qui/components/Midias.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import { View, Image, Text, TouchableOpacity, Linking } from 'react-native'
+import Estilos from './Estilos'
+
+const Midias = () => {
+  return(
+    <View style={ Estilos.container }>
+      <View style={ Estilos.cabecalho }>
+        <Image
+        source={ require('../assets/logoUnilasalle.png') }
+        style={ Estilos.logo }  
+        />
+      </View>
+
+      <View style={ Estilos.subcontainer }>
+        <View style={ [Estilos.coluna, {backgroundColor: 'lightblue'}] }>
+          <TouchableOpacity onPress={ () => Linking.openURL('https://www.unilasalle.edu.br/rj') }>
+            <Image source={ require('../assets/iconeSite.png') }
+              style={ Estilos.icone } />
+          </TouchableOpacity>
+        </View>
+        <View style={ [Estilos.coluna, {backgroundColor: 'yellow'}] }>
+          <TouchableOpacity onPress={ () => Linking.openURL('https://maps.app.goo.gl/U3KebEPfNiS333mz9') }>
+            <Image source={ require('../assets/iconeMaps.png') }
+              style={ Estilos.icone } />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={ Estilos.subcontainer }>
+        <View style={ [Estilos.coluna, {backgroundColor: 'pink'}] }>
+          <TouchableOpacity onPress={ () => Linking.openURL('mailto:joao.voigtlaender@lasalle.org.br') }>
+            <Image source={ require('../assets/iconeEmail.png') }
+              style={ Estilos.icone } />
+          </TouchableOpacity>
+        </View>
+        <View style={ [Estilos.coluna, {backgroundColor: 'lightgreen'}] }>
+          <TouchableOpacity onPress={ () => Linking.openURL('http://api.whatsapp.com/send?phone=5521967803455') }>
+            <Image source={ require('../assets/iconeWhatsapp.png') }
+              style={ Estilos.icone } />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={ Estilos.subcontainer }>
+        <View style={ [Estilos.coluna, {backgroundColor: 'lightblue'}] }>
+          <TouchableOpacity onPress={ () => Linking.openURL('https://www.facebook.com/unilasallerj/?locale=pt_BR') }>
+            <Image source={ require('../assets/iconeFacebook.png') }
+              style={ Estilos.icone } />
+          </TouchableOpacity>
+        </View>
+        <View style={ [Estilos.coluna, {backgroundColor: 'pink'}] }>
+          <TouchableOpacity onPress={ () => Linking.openURL('https://www.instagram.com/unilasallerj/?hl=pt') }>
+            <Image source={ require('../assets/iconeInstagram.png') }
+              style={ Estilos.icone } />
+          </TouchableOpacity>
+        </View>
+      </View>
+
+      <View style={ Estilos.rodape }>
+        <Text style={ Estilos.paragrafo }>2026 - Todos os direitos reservados</Text>
+      </View>
+    </View>
+  )
+}
+
+export default Midias
